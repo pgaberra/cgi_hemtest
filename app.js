e2f2996561e69b9637fa39274d47ddeeab0aae79
Alexander Berglund <albe2003@student.miun.se>
2022-06-09 14:04:02 +0200

    Added POST route that takes text input from user, and sends a response with the ten most frequent words of the given text

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 // include modules
 const express = require('express');
-const path = require('path');
 
 // create express application
 const app = express();
 
+// interpret incoming data as plain text
+app.use(express.text());
+
 // the port the server will accept connections on
 const port = 3000;
+
+function getTheTenMostFrequentWords(words) {
+    return Object.fromEntries(
+        Object.entries(words)
+            .sort(([,a], [,b]) => b-a)  // IMPORTANT that we sort before filtering
+            .filter((element, index) => index < 10)
+    );
+}
+
+app.post('/count', (req, res) => {
+    const userInput = req.body;
+    const wordFrequency = {};
+    for (const word of userInput.split(' ')) {
+        if (word in wordFrequency) {
+            wordFrequency[word]++;
+        } else {
+            wordFrequency[word] = 1;
+        }
+    }
+
+    res.status(200).json(getTheTenMostFrequentWords(wordFrequency));
+})
 
 // start server
 app.listen(port, function() {
